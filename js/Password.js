eb33653eea7f0a83bdf86cf80de50adf69492b96
Geoff Dabu <geoffdabu@gmail.com>
2016-03-26 00:56:25 -0700

    added animations

--- js/Password.js
+++ js/Password.js
@@ -1,7 +1,9 @@
 class Password {
-    constructor(accountName, path) {
+    constructor(accountName, password, path, color) {
         this.name = accountName
+        this.password = password
         this.path = path
+        this.color = color
     }
 }
 
@@ -85,74 +87,146 @@ function plotPassword(startX, startY, sequence, matrix) {
     return [sequenceCoords, plottedPassword]
 }
 
-var colors=['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1']
+var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1']
 
 $(document).ready(function(e) {
     var sequence = ""
     var accounts = []
+    var passwordList = []
 
-    $('#step1-next-button').click(function(e) {
+    $.fn.extend({
+        qcss: function(css) {
+            return $(this).queue(function(next) {
+                $(this).css(css);
+                next();
+            });
+        }
+    });
+
+    $('.step1-next-button').click(function(e) {
         sequence = $('#sequence-input').val()
+        document.getElementById('step3-side-bar').style.visibility = "hidden"
+        $("#password-matrix").empty()
+        $("#text-sequence").empty()
     })
 
-    $('#step2-next-button').click(function(e) {
-    	accounts = []
+    $('.step2-next-button').click(function(e) {
+        accounts = []
         $('.media-icon').each(function(e) {
             // console.log($(this))
             if ($(this).data('toggle') == "on")
                 accounts.push($(this).data('media-type'))
         })
-        console.log(accounts)
+
+        document.getElementById('step3-side-bar').style.visibility = "hidden"
+        $("#password-matrix").empty()
+        $("#text-sequence").empty()
     })
 
     $('#step3-generate-button').click(function(e) {
-    	//get sequence
-    	sequence = $('#sequence-input').val()
+        //get sequence
+        sequence = $('#sequence-input').val()
 
         //generate matrix
-        randomizedMatrix = generateMatrix(6, 6)
-        
+        randomizedMatrix = generateMatrix(9, 9)
+
         //add matrix to dom
         var table = $("#password-matrix")
         table.empty()
 
+        var header = "<tr><td class='matrix-index'><div class='content'>0</div></td>"
+        for (var i = 1; i < randomizedMatrix.length + 1; i++) {
+            header += "<td class='matrix-index'><div class='content'>" + i + "</div></td>"
+        }
+        header += "</tr>"
+
+        table.append(header)
+
+
         for (var i = 0; i < randomizedMatrix.length; i++) {
 
-        	
             var row = "<tr>";
+            row += "<td class='matrix-index'><div class='content'>" + (i + 1) + "</div></td>"
             for (var j = 0; j < randomizedMatrix[0].length; j++) {
-                row += "<td id='password-matrix-" + i + "-" + j + "'><div class='content'>" + randomizedMatrix[i][j] + "</div></td>"
+
+                row += "<td id='password-matrix-" + i + "-" + j + "'><div class='content matrix-password-cell'>" + randomizedMatrix[i][j] + "</div></td>"
             }
             row += "</tr>"
-            console.log(row)
+
             table.append(row)
         }
 
         //display sequence
-        $('#text-sequence').text("Sequence: " + sequence)
-        document.getElementById('text-sequence').style.visibility="visible"
+        $('#text-sequence').text("Sequence: " + sequence.toUpperCase())
+        document.getElementById('text-sequence').style.visibility = "visible"
 
 
-		//add account list to side bar        
-        document.getElementById('step3-side-bar').style.visibility="visible"
+        //add account list to side bar        
+        document.getElementById('step3-side-bar').style.visibility = "visible"
         passwordList = []
 
         //plot the passwords
         $('#step3-side-bar').empty()
+
         for (var i = 0; i < accounts.length; i++) {
-        	var startX = Math.floor((Math.random() * randomizedMatrix.length))
-        	var startY = Math.floor((Math.random() * randomizedMatrix.length))
-        	var password = plotPassword(startX, startY, sequence, randomizedMatrix).slice()
-        	$('#step3-side-bar').append("<div style='background-color:" + colors[i] + ";'><div><h3>"+ accounts[i] +"</h3></div><div>Start: (" + startX + "," + startY + ")</div><div><h4>Password: " + password[1] + "</h4></div></div>")   
-        	      		
-      		for(var j = 0; j < password[0].length; j++){
-      			$('#password-matrix-' + password[0][j].y + '-' + password[0][j].x).addClass("matrix-block-" + accounts[i])
-      			$('#password-matrix-' + password[0][j].y + '-' + password[0][j].x + ' div.content' ).css("background-color", colors[i])
-      		}
+            var startX = Math.floor((Math.random() * randomizedMatrix.length))
+            var startY = Math.floor((Math.random() * randomizedMatrix.length))
+            var passwordDetails = plotPassword(startX, startY, sequence, randomizedMatrix).slice()
+
+
+            $('#step3-side-bar').append("<div class='step3-side-bar-item' data-side-bar-item-index='" + i + "' style='background-color:" + colors[i] + ";'><div><h3>" + accounts[i] + "</h3></div><div>Start: (" + (startX + 1) + "," + (startY + 1) + ")</div><div><h4>PW: " + passwordDetails[1] + "</h4></div></div>")
+
+            for (var j = 0; j < passwordDetails[0].length; j++) {
+                $('#password-matrix-' + passwordDetails[0][j].y + '-' + passwordDetails[0][j].x).addClass("matrix-block-" + accounts[i])
+                $('#password-matrix-' + passwordDetails[0][j].y + '-' + passwordDetails[0][j].x + ' div.content').css("background-color", colors[i])
+            }
+
+            passwordList.push(new Password(accounts[i], passwordDetails[1], passwordDetails[0], colors[i]))
         }
+        $('#step3-side-bar').append("<div class='step3-side-bar-reset' style='background-color:grey;''><div><h3>Show All</h3></div></div>")
 
-        console.log(passwordList)
 
+        $('.step3-side-bar-reset').click(function(e) {
 
+        	$('.matrix-password-cell').css('background-color', 'grey')
+
+            for (var i = 0; i < passwordList.length; i++) {
+            	for (var j = 0; j < passwordList[i].path.length; j++){
+            		$('#password-matrix-' + passwordList[i].path[j].y + '-' + passwordList[i].path[j].x + ' div.content').css({ "background-color": passwordList[i].color })
+            	}
+            }
+        })
+
+        $('.step3-side-bar-item').click(function(e) {
+
+            $('.matrix-password-cell').css('background-color', 'grey')
+
+            var i = $(this).data('side-bar-item-index')
+            console.log(i)
+
+            console.log(passwordList[i])
+
+            // for (var j = 0; j < passwordList[i].path.length; j++) {
+
+            //     $('#password-matrix-' + passwordList[i].path[j].y + '-' + passwordList[i].path[j].x + ' div.content').delay(750).qcss({"background-color": passwordList[i].color})
+            // }
+            var j = 0
+            var loop = setInterval(function() {
+                if (j == passwordList[i].path.length - 1) {
+                    clearInterval(loop);
+                }
+                $('#password-matrix-' + passwordList[i].path[j].y + '-' + passwordList[i].path[j].x + ' div.content')
+                    .delay(0)
+                    .qcss({ "background-color": passwordList[i].color })
+                    .delay(100)
+                    .qcss({ "font-size": '3.5vw' })
+                    .delay(100)
+                    .qcss({ "font-size": '3.0vw' })
+
+                j++
+            }, 100)
+        })
     })
+
+
 })
